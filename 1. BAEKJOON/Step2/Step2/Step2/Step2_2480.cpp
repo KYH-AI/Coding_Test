ce6eb7358dd50074af90b2e6eca8fb378a752986
KYH <pilkyobin123@gmail.com>
2024-12-31 18:41:40 +0900

    [백준 2단계 조건문] 완료

---
+++ 1. BAEKJOON/Step2/Step2/Step2/Step2_2480.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+    int a,b,c,result;
+    cin >> a >> b >> c;
+
+    if(a == b && a == c)
+    {
+        result = 10000+(a*1000);
+    }
+    else if(a == b || a == c)
+    {
+        result = 1000+(a*100);
+    }
+    else if(b == c)
+    {
+        result = 1000+(b*100);
+    }
+    else
+    {
+       result = max(a, max(b,c)) * 100;
+    }
+    
+    cout << result;
+    return 0;
+}
---
+++ 1. BAEKJOON/Step2/Step2/Step2/Step2_2525.cpp
@@ -0,0 +1,17 @@
+﻿/*#include <iostream>
+using namespace std;
+
+int main()
+{
+    // c_h(시)와 c_m(분)을 하나의 분으로 변환한 뒤, m를 더하고 나온 결과 값을 다시 시와 분 으로 변환하는 방법
+    int c_h, c_m, m, temp_m;
+    cin >> c_h >> c_m;
+    cin >> m;
+    
+    temp_m = (c_h * 60) + c_m + m;
+
+    c_h = (temp_m / 60) % 24;
+    c_m = temp_m % 60;
+    cout << c_h << ' ' << c_m;
+    return 0;
+}*/
