ca2e2077bf297ff613e858b6633541f3bed109ce
KYH <pilkyobin123@gmail.com>
2025-02-15 11:33:13 +0900

    [백준 8단계 2 스텝] 완료

---
+++ 1. BAEKJOON/Step8/Step8/Step8/Step8_11005.cpp
@@ -0,0 +1,34 @@
+﻿/*#include <iostream>
+#include <string>
+#include <algorithm>  
+
+using namespace std;
+
+int main()
+{
+    int N, B;
+    string formation;
+
+    cin >> N >> B;
+
+    while(N > 0)
+    {
+        int remainder = N%B;
+        
+        if(remainder >= 10)
+        {
+            formation += 'A' + (remainder - 10);
+        }
+        else
+        {
+            formation += to_string(remainder);
+        }
+
+        N /= B;
+    }
+
+    reverse(formation.begin(), formation.end());
+    cout << formation;
+    
+    return 0;
+}*/
