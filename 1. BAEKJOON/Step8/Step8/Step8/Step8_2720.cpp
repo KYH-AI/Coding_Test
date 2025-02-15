11c13d4f24206ac3647ccba3bd3ecbab24af9d45
KYH-AI <pilkyobin123@gmail.com>
2025-02-15 16:32:56 +0900

    [백준 8단계 3 스텝] 완료

---
+++ 1. BAEKJOON/Step8/Step8/Step8/Step8_2720.cpp
@@ -0,0 +1,39 @@
+﻿/*
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int const QUARTER  = 25, DIME = 10, NICKEL = 5, PENNY = 1;
+    int T;
+    cin >> T;
+    int** result = new int*[T];
+    
+    for(int i=0; i<T; i++) result[i] = new int[4];
+
+    for(int i=0; i<T; i++)
+    {
+        int money;
+        cin >> money;
+        result[i][0] = money / QUARTER;
+        money = money % QUARTER;
+        result[i][1] = money / DIME;
+        money = money % DIME;
+        result[i][2] = money / NICKEL;
+        money = money % NICKEL;
+        result[i][3] = money / PENNY;
+        money = money % PENNY;
+    }
+
+    for(int i=0; i<T; i++)
+    {
+        cout << result[i][0] << ' ' << result[i][1] << ' ' << result[i][2] << ' ' << result[i][3] << '\n';
+    }
+    
+    for(int i=0; i<T; i++) delete[] result[i];
+    delete[] result;
+    
+    return 0;
+}
+*/
