38582155f962b641fa7f5017c3dfd31f64db0c2f
KYH <pilkyobin123@gmail.com>
2025-04-03 19:15:32 +0900

    [백준 12단계 브루트포스] 진행 중

---
+++ 1. BAEKJOON/Step12/Step12/Step12/Step12_1018.cpp
@@ -0,0 +1,58 @@
+﻿/*
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int GetColorCount(char** blackBoard, const char color, const int x, const int y);
+
+int main(){
+    
+    int M, N;
+    vector<int> whiteBlackCount;
+    cin >> M >> N;
+    char ** blackBoard = new char*[M];
+    for(int i=0; i<M; i++) blackBoard[i] = new char[N];
+    
+    for(int i=0; i<M; i++)
+        for(int j=0; j<N; j++)
+            cin >> blackBoard[i][j];
+
+    for(int y=0; y<M-7; y++){
+        for(int x=0; x<N-7; x++){
+            int blackCount = GetColorCount(blackBoard, 'W', x, y);
+            int whiteCount = GetColorCount(blackBoard, 'B', x, y);
+
+            if(blackCount > whiteCount) whiteBlackCount.push_back(whiteCount);
+            else whiteBlackCount.push_back(blackCount);
+        }
+    }
+    
+    for(int i=0; i<M; i++) delete[] blackBoard[i];
+    delete[] blackBoard;
+
+    int min = *min_element(whiteBlackCount.begin(), whiteBlackCount.end());
+    cout << min;
+
+    return 0;
+}
+
+int GetColorCount(char** blackBoard, const char color, const int x, const int y)
+{
+    int colorCount = 0;
+    for(int Y=y; Y<y+8; Y++){
+        for(int X=x; X<x+8; X++){
+            if((Y + X) % 2 == 0) {
+                if(blackBoard[Y][X] != color) colorCount++;
+            }
+            else {
+                if(blackBoard[Y][X] == color) colorCount++;
+            }
+        }
+    }
+    return colorCount;
+}
+*/
+
+
---
+++ 1. BAEKJOON/Step12/Step12/Step12/Step12_2839.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main(){
+    
+    int N, sugar = 0;
+    cin >> N;
+    
+    while(N > 0){
+
+        if(N % 5 == 0){
+            N -= 5;
+            sugar++;
+        }
+        else {
+            N -= 3;
+            sugar++;
+        }
+        if(N < 0) sugar = -1;
+    }
+    
+    cout << sugar;
+    
+    return 0;
+}
